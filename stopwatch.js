dd3d88d37cdfc31236ecb1e3049c0894c9c8a190
Shubham Jain <shubham.jain.1@gmail.com>
2014-03-08 13:00:36 +0530

    Added ouput method(s) and parameters to stopwatch.js

--- stopwatch.js
+++ stopwatch.js
@@ -1,17 +1,33 @@
 var stopwatch = {
-	timeElapsed: null,
-	splitArr: null,
+	isContinuous: true, /* Does the command shows output continously or prints and exits */
+	timeElapsed: 0,
+	splitDiffArr: null,
+	splits: 0,
+	output: "",
 
-	init: function(){
-		this.timeElapsed = 0;
-		this.splitArr = new Array();
+	init: function()
+	{
+		this.splitDiffArr = new Array(0);
+
+		this.output = "# \t\t Time Elapsed\t\tSplit Difference\n";
+		this.output += "----------------------------------------";
 
 		setInterval( function(){
 			this.timeElapsed += 10;
 		}, 10);
 	},
 
-	split: function(){
+	split: function()
+	{
 		this.splitArr.push( this.timeElapsed );
+		this.splitDiffArr.push( this.timeElapsed - this.splitDiffArr [this.splits])
+		this.splits++;
+
+		this.updateOutput();
+	},
+
+	updateOutput: function()
+	{	
+		this.output += this.splits + " \t\t " + this.toFormat( this.timeElapsed[this.splits] ) + "\t\t" + this.toFormat( this.splitArr[this.splits] ) + "\n";
 	}
 }
