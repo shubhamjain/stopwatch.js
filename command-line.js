0fa74e9e7d5b29364eb9ebc70e0a9c15ae183c86
Shubham Jain <shubham.jain.1@gmail.com>
2014-03-08 16:27:24 +0530

    Added default options for command line and template

--- command-line.js
+++ command-line.js
@@ -40,10 +40,35 @@
 
 	}
 
-	$.fn.commandLine = function( outputEle ){
+	//Nano templates by trix.pl
+	function nano(template, data)
+	{
+		return template.replace(/\{([\w\.]*)\}/g, function(str, key) {
+			var keys = key.split("."), v = data[keys.shift()];
+	    	for (var i = 0, l = keys.length; i < l; i++) v = v[keys[i]];
+	    	return (typeof v !== "undefined" && v !== null) ? v : "";
+		});
+	}
+
+	$.fn.commandLine = function( args ){
+
+		var defaults = {
+			outputEle: "pre.output", 		//Element in which the output is to be shown
+			commandRowClass: "command-row", //Class of a new command line inserted ater command finished executing
+			username: "Guest",			
+			computerName: "localhost",
+			blinkingCursor: "span.blinking-cursor", //Used to destroy blinking cursor after command finished exceucting
+			commandRowTemplate: '<div class="command-row"> \
+					                <span class="computer-name">{username}@{computerName}:~$</span> \
+					                <span id="command"></span><span class="blinking-cursor">&#9608;</span> \
+					            </div> \
+					            <pre class="output"></pre>
+					            </div>'
+		}
+
+		$.extend({}, defaults, args);
 	
 		var commandStr = "";
-
 		var commandE = this; //Store the element to jQuery plugin is called, since this is overrided
 
 		// Listen to two events because keypress can't capture Backspace key 
